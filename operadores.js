43ea662858174e5e2da490af6477e558349b676b
Marvin Ramos <mr487223@gmail.com>
2022-10-05 15:53:48 -0600

    operadores en js

---
+++ operadores.js
@@ -0,0 +1,23 @@
+// operadores binarios o matematicos: + - * /
+// el operador + sirve para concatenar 
+// Operadores unitarios. El operador ! negamos algo, ej.:
+console.log(!false)
+// operadores de agisnacion: =
+// ej.:
+var a = 1
+
+// para comparar valores se usa == 0 ===
+// == valida que los valores sean iguales
+// === valida que los valores sean estrictamente iguales. string y string, num y num, etc
+console.log(3=="3")
+console.log(3==="3")
+
+// mas operadores: < menos, > mayor, <= menor o igual, >= mayor o igual
+
+// && (and) ayuda a validar si dos condiciones son verdad
+// || (or) ayuda a validar si una de dos condiciones son verdaderas
+
+// {variable}++ ayuda a incrementar el valor mas 1  
+var edad = 40
+edad++
+edad
