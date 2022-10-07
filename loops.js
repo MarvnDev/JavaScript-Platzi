4c7a4cdb4a73f7dae2f9573d19fecd3055b0fc1e
Marvin Ramos <mr487223@gmail.com>
2022-10-07 13:55:53 -0600

    for of

---
+++ loops.js
@@ -0,0 +1,9 @@
+var students = ["Maria","Sergio","Rosa","Daniel"]
+
+function greetStudents(student){
+    console.log(`Hello ${student}`)
+}
+
+for(var i = 0; i < students.length; i++){
+    greetStudents(students[i])
+}
