ebaa6e250bd834a3a2238c38cf38cca9f8e86e27
Marvin Ramos <mr487223@gmail.com>
2022-10-07 14:28:28 -0600

    aprendiendo sobre como funcionan las funiones construstoras en js

---
+++ funcionConstructora.js
@@ -0,0 +1,16 @@
+function auto(marca, modelo, annio){
+    this.marca = marca // 
+    this.modelo = modelo
+    this.annio = annio
+
+}
+var autoNuevo = new auto("tesla","model 3", 2020) // generando instancia de nuesta funcion constructora
+autoNuevo
+
+var autoNuevoDos = new auto("tesla","modelo x",2018)
+
+autoNuevoDos
+
+var autoNuevoTres = new auto("toyota","corolla", 2020)
+
+autoNuevoTres
