451133da624e19926d07c39c36c3865f0b75f5da
Marvin Ramos <mr487223@gmail.com>
2022-10-07 14:03:01 -0600

    aprendiendo while

--- loops.js
+++ loops.js
@@ -4,6 +4,17 @@ function greetStudents(student){
     console.log(`Hello ${student}`)
 }
 
-for(var i = 0; i < students.length; i++){
-    greetStudents(students[i])
+// for(var i = 0; i < students.length; i++){
+//     greetStudents(students[i])
+// }
+
+// for(var student of students){
+//     greetStudents(student)
+// }
+
+// ----------------while
+while(students.length>0){
+    console.log(students)
+    var student = students.shift()
+    greetStudents(student)
 }
